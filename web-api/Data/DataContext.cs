fbd786a49350045828a641fa9265057a84bd0318
deethesaint <vtduong0912@gmail.com>
2024-08-16 22:23:07 +0700

    add migration

---
+++ web-api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api.Entities;
+
+namespace web_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
